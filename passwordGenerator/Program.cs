51aa58fea0f66663f511a841458c1841e762f362
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2023-02-01 09:45:08 +0100

    feat: init

---
+++ passwordGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using Utils;
+using System;
+
+namespace passwordGenerator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int passwordLength = utils.AskPositifNumberNotNull("Longueur du mot de passe : ");
+
+            string min = "abcdefghijklmnopqrstuvwxyz";
+            string alphabet = min;
+            string password = "";
+            int alphabetLength = alphabet.Length;
+            Random rand = new Random();
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int index = rand.Next(0, alphabetLength);
+                Console.WriteLine($":::index  {index}");
+                password += alphabet[index];
+            }
+
+
+            Console.WriteLine("Mot de passe : " + password);
+
+        }
+    }
+}
---
+++ passwordGenerator/utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+    static class utils
+    {
+        public static int AskPositifNumberNotNull(string question)
+        {
+            return AskPositifNumberBeetween(question, 1, int.MaxValue);
+        }
+
+        public static int AskPositifNumberBeetween(string question, int min, int max)
+        {
+
+            int nombre = AskPositifNumber(question);
+            if ((nombre >= min) && (nombre <= max))
+            {
+                // valide
+                return nombre;
+            }
+            Console.WriteLine("ERREUR : Le nombre doit être compris entre " + min + " et " + max);
+
+            return AskPositifNumberBeetween(question, min, max);
+        }
+
+        public static int AskPositifNumber(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                string reponse = Console.ReadLine() ?? "";
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : Vous devez rentrer un nombre");
+                }
+            }
+        }
+
+    }
+}
